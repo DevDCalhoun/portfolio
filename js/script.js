d5978bd969b4c8a26f465230aa6000431c2ff8c2
Dcalhoun <Dcalhoun102193@gmail.com>
2022-11-29 15:51:34 -0600

    added skill section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#slides').superslides({
         play: 5000,
         animation: 'fade',
@@ -11,5 +11,36 @@ $(document).ready(function() {
         loop: true,
         startDelay: 1000,
         showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 3
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
+        }
+    })
+
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
     });
 });
